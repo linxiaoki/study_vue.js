ac185639d9474f177a8cddea48e7755b688ea842
K_COMPANY_win7 <kisaname@sina.com>
2019-07-05 17:24:12 +0800

    第五章练习未完成

--- 5-内置指令/5-5  练习-购物车/index.js
+++ 5-内置指令/5-5  练习-购物车/index.js
@@ -1,24 +1,24 @@
-var app=new app({
-    el:'app',
+var app=new Vue({
+    el:'#app',
     data:{
         list:[
             {
                 id:1,
                 name:'iPhone7',
                 price:6188,
-                count:1
+                count:27
             },
             {
-                id:2
+                id:2,
                 name:'iPad Pro',
                 price:5888,
-                count:1
+                count:21
             },
             {
                 id:3,
                 name:'MacBook Pro',
                 price:21488,
-                count:1
+                count:36
             }
         ]
     },
@@ -26,16 +26,24 @@ var app=new app({
         totalPrice:function(){
             var total=0;
             var item;
-            foreach(var i=0;i<this.list.length;i++){
-                total += (this.list.price*this.list.count);
+            for(var i=0;i<this.list.length;i++){
+                total += (this.list[i].price*this.list[i].count);
             }
             //带千位分隔符
-            return total.toString();
+            return total.toString().replace(/\B(?=(\d{3})+$)/,',');
         }
     },
     methods:{
-        removeOne:function(index){
-
+        handleAdd:function(index){
+            console.log(index);
+            this.list[index].count++;
+        },
+        handleReduce:function(index){
+            if(this.list[index].count===1) return;
+            this.list[index].count--;
+        },
+        handleRemove:function(index){
+            this.list.splice(index,1);
         }
     }
 });
